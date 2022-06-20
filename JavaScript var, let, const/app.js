5ba711e2b4fd2b7cf21e40fd35a2bf5f3ce18263
Tusar <tusarahmed141@gmail.com>
2022-06-20 09:57:39 +0600

    var can redeclared and re-assigned

---
+++ JavaScript var, let, const/app.js
@@ -0,0 +1,11 @@
+if (true) {
+  var varVariable = 'I am var';
+}
+
+console.log(varVariable);
+
+if (true) {
+  let letVariable = 'I am let'
+  letVariable = 'I am let Again';
+  console.log(letVariable);
+}
