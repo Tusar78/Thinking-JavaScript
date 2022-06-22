cfa324bd8f90288d3717f19b1edd32c9f02a193e
Tusar <tusarahmed141@gmail.com>
2022-06-22 10:49:39 +0600

    Explore closure

--- JavaScript Closures/app.js
+++ JavaScript Closures/app.js
@@ -1,12 +1,35 @@
-const num1 = 10;
-// const num2 = 20;
+// const num1 = 10;
+// // const num2 = 20;
 
-const sum = () => {
-  const num2 = 20;
-  return function () {
-    return num1 + num2
+// const sum = () => {
+//   const num2 = 20;
+//   return function () {
+//     return num1 + num2
+//   }
+// }
+
+// const myFunc = sum();
+// console.dir(myFunc);
+
+// (function () {
+//   console.log('tusar');
+// })()
+
+const stopWatch = () => {
+  const starTime = Date.now();
+
+  const getDelay = () => {
+    console.log(Date.now() - starTime);
   }
+
+  return getDelay;
 }
 
-const myFunc = sum();
-console.dir(myFunc);
+const timer = stopWatch();
+
+for (let i = 0; i < 100000; i++) {
+  const a = Math.random() * 1000;  
+}
+
+timer();
+console.dir(timer)
