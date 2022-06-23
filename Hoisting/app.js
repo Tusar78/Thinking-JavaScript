ad34e3e581e4f28afc76d624888b8925aefb9db8
Tusar <tusarahmed141@gmail.com>
2022-06-23 08:28:26 +0600

    var unspected behavior cause of hoisting

--- Hoisting/app.js
+++ Hoisting/app.js
@@ -3,6 +3,26 @@
  * function
  */
 
-console.log(country);
-var country;
-country = 'Bangladesh';
+// console.log(country);
+// var country;
+// country = 'Bangladesh';
+
+// let language;
+// console.log(language);
+// language = 'JavaScript!'
+
+// let dataType;
+// console.log(dataType);
+// dataType = 'Array';
+
+var LANGUAGE = 'Java';
+var language = 'JavaScript';
+
+function getLanguage () {
+  if (!language) {
+    var language = LANGUAGE;
+  }
+  return language;
+}
+
+console.log(`I love ${getLanguage()}`);
