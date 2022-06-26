e25374bed5ed22c7a8d3e7a741bb8fef1129c2d2
Tusar <tusarahmed141@gmail.com>
2022-06-27 00:12:48 +0600

    Another implicit binding example we see

--- JavaScript this keyword/app.js
+++ JavaScript this keyword/app.js
@@ -23,25 +23,41 @@
 // tusar.play();
 
 // Number two example to understand 'this' implicit bindings
-const playFunction = function (obj) {
-  obj.play = function () {
-    console.log(`${this.name} can playing football!`);
+// const playFunction = function (obj) {
+//   obj.play = function () {
+//     console.log(`${this.name} can playing football!`);
+//   }
+// }
+
+// const tusar = {
+//   name: 'Tusar',
+//   age: 23
+// }
+
+// const rakib = {
+//   name: 'Rakib',
+//   age: 30
+// }
+
+// // Create play function
+// playFunction(tusar);
+// playFunction(rakib);
+
+// tusar.play() 
+// rakib.play() 
+
+// THird example using factory function
+function person (name, age) {
+  return {
+    name,
+    age,
+    play() {
+      console.log(`${this.name} can play crickets!`);
+      console.log(this);
+    }
   }
 }
 
-const tusar = {
-  name: 'Tusar',
-  age: 23
-}
-
-const rakib = {
-  name: 'Rakib',
-  age: 30
-}
-
-// Create play function
-playFunction(tusar);
-playFunction(rakib);
-
-tusar.play() 
-rakib.play() 
+const tusar = person('Tusar', 23)
+console.log(tusar);
+tusar.play()
