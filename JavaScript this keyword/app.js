df243bdb68cf83e16c62c1e82d1d6bd4d99bbef5
Tusar <tusarahmed141@gmail.com>
2022-06-26 23:48:34 +0600

    Start to understanding this keyword

---
+++ JavaScript this keyword/app.js
@@ -0,0 +1,12 @@
+/** JavaScript this keyword rules
+ * 1. Implicit binding
+ * 2. Explicit binding
+ * 3. new keyword
+ * 4. window
+ */
+
+var printPlayerName = function (name) {
+  console.log(name);
+}
+
+printPlayerName('Tusar');
