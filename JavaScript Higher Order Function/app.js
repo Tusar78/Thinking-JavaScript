454a52cd4088f251190f45a0893a01bb15f03870
Tusar <tusarahmed141@gmail.com>
2022-06-27 14:13:45 +0600

    Traditional filter

--- JavaScript Higher Order Function/app.js
+++ JavaScript Higher Order Function/app.js
@@ -28,6 +28,37 @@
 // console.log(result);
 
 // Using higher order function
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const result = numbers.map((number) => `${number} x 2 = ${number * 2}`);
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const result = numbers.map((number) => `${number} x 2 = ${number * 2}`);
+// console.log(result);
+
+// Array filter as a traditional way
+const players = [
+  {
+    name: 'Sakib',
+    avg: 38.9
+  },
+  {
+    name: 'Mahmudulla',
+    avg: 36.4
+  },
+  {
+    name: 'Mushfiq',
+    avg: 35.3
+  },
+  {
+    name: 'Tamim',
+    avg: 37.8
+  }
+]
+
+const result = [];
+
+for (let i = 0; i < players.length; i++) {
+  const graterAvg = players[i].avg;
+  if (graterAvg >= 37) {
+    result.push(players[i])
+  }    
+}
+
 console.log(result);
