a8056acc26c800660321fc8a18c1bae6f55cb61c
Tusar <tusarahmed141@gmail.com>
2022-06-27 13:58:42 +0600

    When we are not using higher order function

--- JavaScript Higher Order Function/app.js
+++ JavaScript Higher Order Function/app.js
@@ -7,12 +7,22 @@
 // console.dir(hello)
 
 // Higher Order Function
-function hello() {
-  console.log('Hello world');
-  return function() {
-    console.log('Hello World 2');
-  }
+// function hello() {
+//   console.log('Hello world');
+//   return function() {
+//     console.log('Hello World 2');
+//   }
+// }
+
+// const result = hello();
+// result()
+
+// Not using higher order function
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const result = [];
+
+for (let i = 0; i < numbers.length; i++) {
+  result.push(`${numbers[i]} x 2 = ${numbers[i] * 2}`);  
 }
 
-const result = hello();
-result()
+console.log(result);
