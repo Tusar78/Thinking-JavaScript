bfe0399b200ac87aaba83408c9ad97842e2caa73
Tusar <tusarahmed141@gmail.com>
2022-06-29 10:48:36 +0600

    Complete recursion

--- Recursion in JavaScript/app.js
+++ Recursion in JavaScript/app.js
@@ -2,18 +2,24 @@
 // const n = 10;
 
 // for (let i = 0; i <= n; i++) {
-//   total += i;  
+//   total += i;
 // }
 
 // console.log(total);
 
 // f(n-1) + n = f(n)
-function sum(n) {
-  if (n === 0) {
-    return 0;
-  } else {
-    return sum(n - 1) + n
-  }
-}
+// function sum(n) {
+//   if (n === 0) {
+//     return 0;
+//   } else {
+//     return sum(n - 1) + n
+//   }
+// }
 
-console.log(sum(10));
+// console.log(sum(3));
+
+// 1 + 2 + 3 + 4 . . . . + n
+// n(n+1) / 2
+
+const n = 3;
+console.log((n * (n + 1)) / 2);
