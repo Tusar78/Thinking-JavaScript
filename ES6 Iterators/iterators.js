340e88c858e3ea00a0485511b75079f039d06095
Tusar <tusarahmed141@gmail.com>
2022-07-03 20:43:33 +0600

    Create custom iteratore

--- ES6 Iterators/iterators.js
+++ ES6 Iterators/iterators.js
@@ -1,13 +1,21 @@
 const range = (start, end, step) => {
+  let current = start;
   return {
-    [Symbol.iterator] : function () {
+    [Symbol.iterator]: function () {
       return {
-        next () {
-          
-        }
-      }
-    }
-  }
-}
+        next() {
+          let result;
+          if (current <= end) {
+            result = { done: false, value: current };
+            current += step;
+            return result;
+          }
+          return { done: true };          
+        },
+      };
+    },
+  };
+};
 
-const number = [...range(1, 50, 2)]
+const number = [...range(1, 50000, 1)];
+console.log(number);
