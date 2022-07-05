296bb1395ad60b8b66e77b1c2f1f62176b40d784
Tusar <tusarahmed141@gmail.com>
2022-07-06 00:26:36 +0600

    Beauty of generator

--- ES6 Generator/app.js
+++ ES6 Generator/app.js
@@ -19,34 +19,46 @@ console.log(iterator.next().value);
 // const iterator = generator();
 // console.dir(iterator);
 
-const numbers = {
-  value1: 1,
-  value2: 2,
-  value3: 3,
-  value4: 4,
-};
+// const numbers = {
+//   value1: 1,
+//   value2: 2,
+//   value3: 3,
+//   value4: 4,
+// };
 
-// Make iterator to object
-Object.prototype[Symbol.iterator] = function () {
-  const entries = Object.entries(this);
-  let count = entries.length;
-  let index = 0;
+// // Make iterator to object
+// Object.prototype[Symbol.iterator] = function () {
+//   const entries = Object.entries(this);
+//   let count = entries.length;
+//   let index = 0;
 
-  return {
-    next() {      
-      if (count > 0) {
-        let result = { done: false, value: entries[index][1] };
-        count--;
-        index++;
-        return result;
-      } else {
-        return { done: true, value: undefined };
-      }
-    },
-  };
-};
+//   return {
+//     next() {      
+//       if (count > 0) {
+//         let result = { done: false, value: entries[index][1] };
+//         count--;
+//         index++;
+//         return result;
+//       } else {
+//         return { done: true, value: undefined };
+//       }
+//     },
+//   };
+// };
 
 
-for (let number of numbers) {
-  console.log(number);
+// for (let number of numbers) {
+//   console.log(number);
+// }
+
+
+// Generator Control Flow
+function* generator(a, b) {
+  let sum = yield a + b;  
+  let sum2 = yield a + b;  
 }
+
+const gen = generator(10, 20);
+console.log(gen.next());
+console.log(gen.next()); 
+console.log(gen.next()); 
