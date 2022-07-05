c1c051fd17da226de968b313c21daef45167d065
Tusar <tusarahmed141@gmail.com>
2022-07-05 22:23:09 +0600

    Revising iterator

---
+++ ES6 Generator/app.js
@@ -0,0 +1,7 @@
+// Revise Iterator
+const numbers = [1, 23, 4];
+const iterator = numbers[Symbol.iterator]();
+console.log(iterator.next().value);
+console.log(iterator.next().value);
+console.log(iterator.next().value);
+console.log(iterator.next().value);
