b1b8dbfb9e29bc88d8ff09a90814cecb493fefe0
Tusar <tusarahmed141@gmail.com>
2022-07-05 23:40:13 +0600

    Make object as a iterable using iterator terminology

--- ES6 Generator/app.js
+++ ES6 Generator/app.js
@@ -20,12 +20,33 @@ console.log(iterator.next().value);
 // console.dir(iterator);
 
 const numbers = {
-  value: 1,
-  value: 2,
-  value: 3,
-  value: 4
-}
+  value1: 1,
+  value2: 2,
+  value3: 3,
+  value4: 4,
+};
 
-for (const number of numbers) {
+// Make iterator to object
+Object.prototype[Symbol.iterator] = function () {
+  const entries = Object.entries(this);
+  let count = entries.length;
+  let index = 0;
+
+  return {
+    next() {      
+      if (count > 0) {
+        let result = { done: false, value: entries[index][1] };
+        count--;
+        index++;
+        return result;
+      } else {
+        return { done: true, value: undefined };
+      }
+    },
+  };
+};
+
+
+for (let number of numbers) {
   console.log(number);
 }
