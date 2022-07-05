47b2e345a6af36e62c576b9753b606e975ee0118
Tusar <tusarahmed141@gmail.com>
2022-07-05 22:57:25 +0600

    Generator write

--- ES6 Generator/app.js
+++ ES6 Generator/app.js
@@ -1,7 +1,20 @@
 // Revise Iterator
+/*
 const numbers = [1, 23, 4];
 const iterator = numbers[Symbol.iterator]();
 console.log(iterator.next().value);
 console.log(iterator.next().value);
 console.log(iterator.next().value);
 console.log(iterator.next().value);
+*/
+
+function *generator() {
+  yield 1;
+  yield 2;
+  return 'I am finished'
+  yield 3;
+  yield 4;
+}
+
+const iterator = generator();
+console.dir(iterator);
